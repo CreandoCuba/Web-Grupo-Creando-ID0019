62fab8edc89e26b10c840471e13f6f80e4a9f077
Kevin <kevinpinodelsol@gmail.com>
2024-03-30 19:44:12 -0400

    Creacion de la Aplicacion de React
    Creada la aplicación de React
    Instaladas dependencias para usar Chakra UI
    Puesto un NavBar de Chakra y Cambiado el color a naranja

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import WithSubnavigation from "./NavBar";
+
+function App() {
+      return (
+          <div>
+                <WithSubnavigation></WithSubnavigation>
+          </div>
+      )
+}
+
+export default App;
