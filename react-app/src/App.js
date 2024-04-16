0456916790d63eee111fadc26e68da413671773c
Kevin <kevinpinodelsol@gmail.com>
2024-04-15 22:12:39 -0400

    Cambios en los links
    Ahora los links llevan a sitios que si existen, y los que no, fueron eliminados
    - ajustados los link de la lista de servicios
    - eliminado el leer mas de sobre nosotros
    - eliminado el ver todos de la lista de servicios

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -13,6 +13,9 @@ import Diseno from "./pages/Services/Diseno";
 import Turismo from "./pages/Services/Turismo";
 import Contabilidad from "./pages/Services/Contabilidad";
 import AudioVisuales from "./pages/Services/AudioVisuales";
+
+const mainImgRoute = 'images/services/'
+
 const pages = [
     {
         path:'/',
@@ -33,49 +36,96 @@ const pages = [
                 path:'/administraciondeempresas',
                 label: 'Administracion de Empresas',
                 subLabel: 'Estrategia, Logistica y Planificacion',
-                element: <AdministracionDeEmpresas/>
+                element: <AdministracionDeEmpresas/>,
+                imgRoute: mainImgRoute +'Administración de Empresas.png',
+                subList: [
+                    'Diseño de Modelo de Negocios.',
+                    'Planeación Estratégica',
+                    'Logística'
+                ]
             },
             {
                 path: '/desarrolloLocal',
                 label: 'Desarrollo Local',
                 subLabel: 'Elaboracion de estrategias y busqueda de oportunidades',
-                element: <DesarrolloLocal/>
+                element: <DesarrolloLocal/>,
+                imgRoute: mainImgRoute+'Desarrollo Local.png',
+                subList: [
+                    'Identidad, Imagen y Marketing de ciudades o localidades.',
+                    'Diseño y desarrollo de marcas.'
+                ]
             },
             {
                 path: '/Diseno',
                 label: 'Diseño',
                 subLabel: 'Logos, interiores, personajes y Stands',
-                element: <Diseno/>
+                element: <Diseno/>,
+                imgRoute: mainImgRoute +'Diseño.png',
+                subList: [
+                    'Diseño de Personaje o Mascota',
+                    'Diseño de interiores',
+                    'Diseño de infografías.'
+                ]
             },
             {
                 path: '/Informatica',
                 label: 'Informatica',
                 subLabel: 'Paginas Web y Automatizacion de procesos',
-                element: <Informatica/>
+                element: <Informatica/>,
+                imgRoute: mainImgRoute +'Informática.png',
+                subList: [
+                    'Creación y Gestión de páginas web.',
+                    'Informatización de procesos.',
+                    'Seguridad informática.'
+                ]
             },
             {
                 path: '/Turismo',
                 label: 'Turismo',
                 subLabel: 'Diseño de productos y destinos turisticos',
-                element: <Turismo/>
+                element: <Turismo/>,
+                imgRoute: mainImgRoute+'Turismo.png',
+                subList: [
+                    'Diseño de productos turísticos.',
+                    'Diseño de puntos náuticos.',
+                    'Diseño de souvenirs.'
+                ]
             },
             {
                 path: '/Audiovisuales',
                 label: 'Audiovisuales',
                 subLabel: 'Creacion de Fotografias y Videos profesionales',
-                element: <AudioVisuales/>
+                element: <AudioVisuales/>,
+                imgRoute: mainImgRoute +'Fotografía.png',
+                subList: [
+                    'Fotos publicitarias de estudio.',
+                    'Fotos publicitarias de exteriores.',
+                    'Colchón fotográfico publicitario.'
+                ]
             },
             {
                 path: '/Marketing',
                 label: 'Marketing',
                 subLabel: 'Mejora tu imagen ante tus clientes',
-                element: <Marketing/>
+                element: <Marketing/>,
+                imgRoute: mainImgRoute +'Marketing y Comunicación.png',
+                subList: [
+                    'Investigación de mercados',
+                    'Conceptualización de productos.',
+                    'Diseño de productos.'
+                ]
             },
             {
                 path: '/Contabilidad',
                 label: 'Contabilidad y Finanzas',
                 subLabel: '',
-                element: <Contabilidad/>
+                element: <Contabilidad/>,
+                imgRoute: mainImgRoute +'Contabilidad y Finanzas.png',
+                subList:[
+                    'Teneduría de libros',
+                    'Finanzas corporativas',
+                    'Estudios de factibilidad'
+                ]
             }
         ]
     },
--- react-app/src/sections/Service.jsx
+++ react-app/src/sections/Service.jsx
@@ -1,79 +1,9 @@
+import {pages} from "../App";
+import {Link} from "react-router-dom";
 
 const mainImgRoute = 'images/services/'
-const services = [
-    {
-        name:'Administración de Empresas',
-        imgRoute: mainImgRoute +'Administración de Empresas.png',
-        subList: [
-            'Diseño de Modelo de Negocios.',
-            'Planeación Estratégica',
-            'Logística'
-        ]
-    },
-    {
-        name: 'Contabilidad y Finanzas',
-        imgRoute: mainImgRoute +'Contabilidad y Finanzas.png',
-        subList:[
-            'Teneduría de libros',
-            'Finanzas corporativas',
-            'Estudios de factibilidad'
-        ]
-    },
-    {
-        name: 'Desarrollo Local',
-        imgRoute: mainImgRoute+'Desarrollo Local.png',
-        subList: [
-            'Identidad, Imagen y Marketing de ciudades o localidades.',
-            'Diseño y desarrollo de marcas.'
-        ]
-    },
-    {
-        name: 'Diseño',
-        imgRoute: mainImgRoute +'Diseño.png',
-        subList: [
-            'Diseño de Personaje o Mascota',
-            'Diseño de interiores',
-            'Diseño de infografías.'
-        ]
-    },
-    {
-        name: 'Fotografía',
-        imgRoute: mainImgRoute +'Fotografía.png',
-        subList: [
-            'Fotos publicitarias de estudio.',
-            'Fotos publicitarias de exteriores.',
-            'Colchón fotográfico publicitario.'
-        ]
-    },
-    {
-        name: 'Informatica',
-        imgRoute: mainImgRoute +'Informática.png',
-        subList: [
-            'Creación y Gestión de páginas web.',
-            'Informatización de procesos.',
-            'Seguridad informática.'
-        ]
-    },
-    {
-        name: 'Marketing y Comunicacion',
-        imgRoute: mainImgRoute +'Marketing y Comunicación.png',
-        subList: [
-            'Investigación de mercados',
-            'Conceptualización de productos.',
-            'Diseño de productos.'
-        ]
-    },
-    {
-        name: 'Turismo',
-        imgRoute: mainImgRoute+'Turismo.png',
-        subList: [
-            'Diseño de productos turísticos.',
-            'Diseño de puntos náuticos.',
-            'Diseño de souvenirs.'
-        ]
-    }
-]
 export default () => {
+const services = pages[2].children
     return (
 
         <section className="service_section layout_padding">
@@ -98,21 +28,16 @@ export default () => {
                                         </div>
                                         <div className="detail-box">
                                             <h5>
-                                                {value.name}
+                                                {value.label}
                                             </h5>
                                             <ul>{value.subList?.map((value) => {return <li>{value}</li>} )}</ul>
-                                            <a href="">
+                                            <a href={value.path}>
                                                 Leer más
                                             </a>
                                         </div>
                                     </div>
                                 </div>
                         )})}
-                    </div>
-                    <div className="btn-box">
-                        <a href="">
-                            Ver todos
-                        </a>
                     </div>
                 </div>
             </div>
