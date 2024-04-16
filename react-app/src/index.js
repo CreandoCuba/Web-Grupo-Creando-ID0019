d276167cb6b918910b687fcd7524806682f21ed4
Kevin <kevinpinodelsol@gmail.com>
2024-04-16 14:55:28 -0400

    Cambio del icono de la pagina
    Tambien se comento el basename del Router para poder ir trabajando con el

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   	<React.StrictMode>
 		<ChakraProvider>
-			<BrowserRouter basename='/grupocreativo.github.io'>
+			<BrowserRouter basename={'/'//grupocreativo.github.io'
+			}>
 				<App />
 			</BrowserRouter>
 		</ChakraProvider>
