bdc6545577d7f39c1b047bf544bb6bf270068f8d
Kevin <kevinpinodelsol@gmail.com>
2024-04-17 17:45:16 -0400

    Cambio de los derechos del Footer

--- react-app/src/sections/Footer.jsx
+++ react-app/src/sections/Footer.jsx
@@ -5,8 +5,7 @@ export default () => {
         <section className="footer_section">
             <div className="container">
                 <p>
-                    &copy; <span id="displayYear"></span> All Rights Reserved By
-                    <a href="https://html.design/">Free Html Templates</a>
+                    &copy; <span id="displayYear"></span> Todos los derechos reservados: Creando
                 </p>
             </div>
         </section>
