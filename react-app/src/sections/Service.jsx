50a80534998af4f195b79fe4d743bbb038d7e13f
Kevin <kevinpinodelsol@gmail.com>
2024-05-30 14:20:52 -0400

    Alineacion de las presentaciones de servicios
    Se quito el link de leer mas de los elementos
    Se puso el titulo de los elementos como el link y se subrayó para que se note

--- react-app/src/sections/Service.jsx
+++ react-app/src/sections/Service.jsx
@@ -1,7 +1,7 @@
 import {pages} from "../App";
 import {Link} from "react-router-dom";
 import {Slide} from 'react-reveal';
-import {Container, SimpleGrid} from "@chakra-ui/react";
+import {Center, Container, Heading, SimpleGrid, Wrap} from "@chakra-ui/react";
 
 const Service = () => {
 const services = pages[2].children
@@ -23,19 +23,19 @@ const services = pages[2].children
                     <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }}>
                         {services.map((value,index) => {return(
                             <Slide up duration={500} delay={100*(index%4)} fraction={0.5}>
-                                <Container className="col-md-4 " key={index}>
+                                <Container className="col-md-4" key={index}>
                                     <div className="box ">
                                         <div className="img-box">
                                             <img src={value.imgRoute} alt="Icono Representativo"/>
                                         </div>
                                         <div className="detail-box">
-                                            <h5>
-                                                {value.label}
-                                            </h5>
-                                            <ul>{value.subList?.map((value, index) => {return <li className='noDeco' key={index}>{value}</li>} )}</ul>
                                             <Link to={value.path}>
-                                                Leer más
+                                            <Heading fontSize='20px' textDecor='underline'>
+                                                {value.label}
+                                            </Heading>
                                             </Link>
+
+                                            <ul>{value.subList?.map((value, index) => {return <li className='noDeco' key={index}>{value}</li>} )}</ul>
                                         </div>
                                     </div>
                                 </Container>
